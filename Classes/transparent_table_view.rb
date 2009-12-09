d763ed4a35839051f1d6ac84f7797c9862efac03
Arthur Zapparoli <arthurzap@gmail.com>
2009-12-09 03:43:03 -0200

    Added a TransparentTableView class

---
+++ Classes/transparent_table_view.rb
@@ -0,0 +1,11 @@
+class TransparentTableView < NSTableView
+
+	def	awakeFromNib
+		self.backgroundColor = NSColor.clearColor
+	end
+	
+	def	drawBackgroundInClipRect(clipRect)
+		# don't draw a background rect
+	end
+
+end
