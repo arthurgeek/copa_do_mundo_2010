5366a51546cf31574c8e9d1034a1938d5bb9112d
Arthur Zapparoli <arthurzap@gmail.com>
2009-12-09 02:17:51 -0200

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Copa do Mundo 2010
+//
+//  Created by Arthur Zapparoli on 09/12/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
